ba0588b2c90a914c9af97b6a2bf7b9b54d6267dd
mariesaoconnell <mariesa.oconnell@gmail.com>
2023-04-26 18:00:11 -0400

    Embed lesson completed

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ learning how to make a discord bot using Discord.js v14
 
 utilizing: https://www.youtube.com/watch?v=KZ3tIGHU314&t=1s
 # issa-bot-discord-bot
+
+# Slash Commands
+
+ # Embeds:
+> https://www.youtube.com/watch?v=u5_a-lQlv6A
+> https://discordjs.guide/popular-topics/embeds.html#embed-preview
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 require('dotenv/config');
 
-const { Client, IntentsBitField } = require('discord.js');
+const { Client, IntentsBitField, EmbedBuilder } = require('discord.js');
 
 // CLIENT = BOT.. INITIALIZE CLIENT
 const client = new Client({
@@ -14,16 +14,55 @@ const client = new Client({
 });
 
 // EVENT LISTENERS
+
 // bot on when ready will console log the bot's user name is online
 client.on('ready', (c) => {
 	console.log(`✅ ${c.user.username} is online! `);
 });
 
-// listens for a message to be sent
-client.on('messageCreate', (msg) => {
-  if(msg.author.bot) return;
 
-	msg.reply(`Hello ${msg.author}, how are you?`);
+client.on('interactionCreate', (interaction) => {
+	if (!interaction.isChatInputCommand()) return;
+
+	if (interaction.commandName === 'hey') {
+		interaction.reply(`Hey ${interaction.member}!`);
+	}
+
+	if (interaction.commandName === 'embed') {
+		const embed = new EmbedBuilder()
+			.setTitle('Embed Title')
+			.setDescription('This is an embed description.')
+			.setColor('Random')
+			.addFields({
+				name: 'Field Title',
+				value: 'Some random value',
+				inline: true,
+			});
+		interaction.reply({ embeds: [embed] });
+	}
+});
+
+client.on('messageCreate', (msg) => {
+	if (msg.content === 'embed') {
+		const embed = new EmbedBuilder()
+			.setTitle('Embed Title')
+			.setDescription('This is an embed description.')
+			.setColor('Random')
+			.addFields(
+				{
+					name: 'Field Title',
+					value: 'Some random value',
+					inline: true,
+				},
+				{
+					name: 'Field Title',
+					value: 'Some random value',
+					inline: true,
+				}
+			);
+
+		msg.channel.send({ embeds: [embed]})
+	}
 });
 
 client.login(process.env.TOKEN);
